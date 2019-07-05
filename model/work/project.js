bdc38c616243be62375a33cd8136aa6867ca4b09
Brian Bixby <brianbixby0@gmail.com>
2019-07-04 18:33:05 -0700

    changed schema for c projects

--- model/work/project.js
+++ model/work/project.js
@@ -6,15 +6,16 @@ const projectSchema = mongoose.Schema({
   url: {type: String, required: true },
   name: {type: String, required: true },
   shortDesc: {type: String, required: true },
-  fullDesc: {type: String, required: true },
+  fullDesc: {type: String },
   fullDesc1: {type: String },
   fullDesc2: {type: String },
   keyTech: {type: String },
   site: {type: String },
-  github: {type: String, required: true },
+  github: {type: String },
   image: {type: String, required: true },
   bwImage: {type: String },
   bigImage: {type: String },
+  projects: [{projectName: String, projectGithub: String, projectDesc: String }],
 });
 
 module.exports = mongoose.model('project', projectSchema);
