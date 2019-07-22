c06c131d95107990f25ceaf0978ef66a178a15a0
Brian Bixby <brianbixby0@gmail.com>
2019-07-22 15:59:26 -0700

    added compression

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('dotenv').config();
+const compression = require('compression');
 const express = require('express');
 const debug = require('debug')('portfoliobackend:server');
 const mongoose = require('mongoose');
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false });
 
+app.use(compression());
 app.use(allRoutes);
 
 const server = module.exports = app.listen(PORT, () => {
