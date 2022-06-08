2442100fd4eeee2a4057b9839329c94d450c6a07
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-06-08 09:43:26 -0700

    cors

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -7,20 +7,20 @@ const messageRouter = require('./contact/message-router.js');
 const projectRouter = require('./work/project-router.js');
 const errors = require('./../lib/error-middleware.js');
 
-const whiteList = [process.env.CORS_ORIGINS, process.env.CORS_ORIGINS1, process.env.CORS_ORIGINS2, process.env.CORS_ORIGINS3, process.env.CORS_ORIGINS4];
+// const whiteList = [process.env.CORS_ORIGINS, process.env.CORS_ORIGINS1, process.env.CORS_ORIGINS2, process.env.CORS_ORIGINS3, process.env.CORS_ORIGINS4];
 
 module.exports = new Router()
   .use([
-    cors({
-      credentials: true,
-      origin: (origin, cb) => {
-        if (whiteList.indexOf(origin) !== -1) {
-          cb(null, true);
-        } else {
-          cb(new Error('Not allowed by CORS'));
-        }
-      },
-    }),
+    // cors({
+    //   credentials: true,
+    //   origin: (origin, cb) => {
+    //     if (whiteList.indexOf(origin) !== -1) {
+    //       cb(null, true);
+    //     } else {
+    //       cb(new Error('Not allowed by CORS'));
+    //     }
+    //   },
+    // }),
     messageRouter,
     projectRouter,
     errors,
--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes/allRoutes');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 db.once('open', (err, resp) => {
