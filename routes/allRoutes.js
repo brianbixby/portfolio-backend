7b4c136a824a2b46ff8c4d6e531ff6995406e645
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-06-08 11:39:48 -0700

    small updates

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,26 +1,13 @@
 'use strict';
 
 const Router = require('express').Router;
-const cors = require('cors');
 
 const messageRouter = require('./contact/message-router.js');
 const projectRouter = require('./work/project-router.js');
 const errors = require('./../lib/error-middleware.js');
 
-// const whiteList = [process.env.CORS_ORIGINS, process.env.CORS_ORIGINS1, process.env.CORS_ORIGINS2, process.env.CORS_ORIGINS3, process.env.CORS_ORIGINS4];
-
 module.exports = new Router()
   .use([
-    // cors({
-    //   credentials: true,
-    //   origin: (origin, cb) => {
-    //     if (whiteList.indexOf(origin) !== -1) {
-    //       cb(null, true);
-    //     } else {
-    //       cb(new Error('Not allowed by CORS'));
-    //     }
-    //   },
-    // }),
     messageRouter,
     projectRouter,
     errors,
