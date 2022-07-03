551c2dfa8924d5d918628a1e99602d22c437b580
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-07-03 10:07:52 -0700

    added compression

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 "use strict";
 
 require('dotenv').config();
+const compression = require('compression');
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes/allRoutes');
 const cors = require('cors');
 
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
